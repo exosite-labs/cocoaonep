25258b4e3007227150a31247f3335a632bb2f187
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2014-02-20 12:58:03 -0600

    Adding a project file.

---
+++ Project/CocoaOnePTests/CocoaOnePTests.m
@@ -0,0 +1,34 @@
+//
+//  CocoaOnePTests.m
+//  CocoaOnePTests
+//
+//  Created by Michael Conrad Tadpol Tilstra.
+//  Copyright (c) 2014 Exosite. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CocoaOnePTests : XCTestCase
+
+@end
+
+@implementation CocoaOnePTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
