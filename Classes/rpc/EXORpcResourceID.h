c5bf6b8cee59b80e86b2b5fb55f590f812cf3011
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2014-05-15 15:54:07 -0500

    Resource ID API docs.

--- Classes/rpc/EXORpcResourceID.h
+++ Classes/rpc/EXORpcResourceID.h
@@ -7,15 +7,63 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ Resource Identifier
+ */
 @interface EXORpcResourceID : NSObject <NSCopying>
 
-+ (EXORpcResourceID*)resourceIDByAlias:(NSString*)alias;
+/**
+ Directly identify a resource by its RID string.
+ 
+ The resource must be in the calling client's subhierarchy.
+
+ @param rid The 40 character resource identifier
+ @return The resource identifier
+ */
 + (EXORpcResourceID*)resourceIDByRID:(NSString*)rid;
+
+/**
+ Identify an immediate child of the calling client by alias.
+
+ @param alias The alias to look up.
+ @return The resource identifier
+ */
++ (EXORpcResourceID*)resourceIDByAlias:(NSString*)alias;
+
+/**
+ Identify the calling client itself.
+ @return The resource identifier
+ */
 + (EXORpcResourceID*)resourceIDAsSelf;
 
-- (instancetype)init;
-- (instancetype)initWithAlias:(NSString*)alias;
+/**
+ Directly identify a resource by its RID string.
+
+ The resource must be in the calling client's subhierarchy.
+
+ @param rid The 40 character resource identifier
+ @return The resource identifier
+ */
 - (instancetype)initWithRID:(NSString*)rid;
 
+/**
+ Identify an immediate child of the calling client by alias.
+
+ @param alias The alias to look up.
+ @return The resource identifier
+ */
+- (instancetype)initWithAlias:(NSString*)alias;
+
+/**
+ Identify the calling client itself.
+ @return The resource identifier
+ */
+- (instancetype)init;
+
+/**
+ Return this auth as a plist that can be converted into JSON.
+
+ @return JSON ready dictionary of values.
+ */
 - (id)plistValue;
 @end
