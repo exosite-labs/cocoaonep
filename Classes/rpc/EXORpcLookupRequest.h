ad09ab69ce5b16e0aaf1a077ebf8c01abe8f4d91
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2014-06-09 10:55:14 -0500

    Docs for Lookup request

--- Classes/rpc/EXORpcLookupRequest.h
+++ Classes/rpc/EXORpcLookupRequest.h
@@ -7,6 +7,9 @@
 
 #import "EXORpcRequest.h"
 
+/**
+ Lookup types
+ */
 enum EXORpcLookupType_e {
     EXORpcLookupTypeAlias,
     EXORpcLookupTypeOwner,
@@ -14,13 +17,38 @@ enum EXORpcLookupType_e {
 };
 typedef enum EXORpcLookupType_e EXORpcLookupType_t;
 
-typedef void(^EXORpcLookupRequestComplete)(NSString *resut, NSError *err);
+/**
+ Callback for the results of a lookup
+ @param result The RID that was looked up
+ @param error nil on Success, otherwise the error
+ */
+typedef void(^EXORpcLookupRequestComplete)(NSString *result, NSError *error);
 
+/**
+ Look up a Resource ID by alias, owned Resource ID, or share activation code.
+ */
 @interface EXORpcLookupRequest : EXORpcRequest <NSCopying>
-@property(assign,nonatomic,readonly) EXORpcLookupType_t type;
-@property(copy,nonatomic,readonly) NSString *item;
+@property(assign,nonatomic,readonly) EXORpcLookupType_t type;  /// What type of thing to lookup
+@property(copy,nonatomic,readonly) NSString *item;  /// The key to lookup
 @property(copy,nonatomic,readonly) EXORpcLookupRequestComplete complete;
 
+/**
+ Create a lookup request
+ 
+ @param type The type of lookup to do
+ @param item The key of the thing to lookup
+ @param complete The callback to collect the results
+ @return The Lookup Request
+ */
 + (EXORpcLookupRequest*)lookupWithType:(EXORpcLookupType_t)type item:(NSString*)item complete:(EXORpcLookupRequestComplete)complete;
+
+/**
+ Initialize a lookup request
+
+ @param type The type of lookup to do
+ @param item The key of the thing to lookup
+ @param complete The callback to collect the results
+ @return The Lookup Request
+ */
 - (instancetype)initWithType:(EXORpcLookupType_t)type item:(NSString*)item complete:(EXORpcLookupRequestComplete)complete;
 @end
