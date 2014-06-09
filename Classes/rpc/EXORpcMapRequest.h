d98860dd88555667bce5dd6f6d1d620405acb5f1
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2014-06-09 12:03:52 -0500

    Docs for map request

--- Classes/rpc/EXORpcMapRequest.h
+++ Classes/rpc/EXORpcMapRequest.h
@@ -7,10 +7,30 @@
 
 #import "EXORpcRequest.h"
 
+/**
+ Creates an alias for a resource.
+ */
 @interface EXORpcMapRequest : EXORpcRequest <NSCopying>
-@property(strong,nonatomic,readonly) NSString *aliasName;
-@property(copy,nonatomic,readonly) EXORpcRequestComplete complete;
+@property(strong,nonatomic,readonly) NSString *aliasName; /// Name to use for alias
+@property(copy,nonatomic,readonly) EXORpcRequestComplete complete; /// Callback when complete
 
+/**
+ Create a mapping
+ 
+ @param rid The target resource for the alias
+ @param alias The name of the alias
+ @param complete Callback on complete
+ @return THe map request
+ */
 + (EXORpcMapRequest*)mapWithRID:(EXORpcResourceID *)rid to:(NSString*)alias complete:(EXORpcRequestComplete)complete;
+
+/**
+ Initialize a mapping
+
+ @param rid The target resource for the alias
+ @param alias The name of the alias
+ @param complete Callback on complete
+ @return THe map request
+ */
 - (instancetype)initWithRID:(EXORpcResourceID *)rid to:(NSString*)alias complete:(EXORpcRequestComplete)complete;
 @end
