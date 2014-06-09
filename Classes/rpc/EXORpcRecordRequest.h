4fdcc48877e3e989682fa0554bd4b19245ddf449
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2014-06-09 12:18:23 -0500

    Docs for record request

--- Classes/rpc/EXORpcRecordRequest.h
+++ Classes/rpc/EXORpcRecordRequest.h
@@ -8,12 +8,31 @@
 #import "EXORpcRequest.h"
 #import "EXORpcValue.h"
 
+/**
+ Records a list of historical entries
+ */
 @interface EXORpcRecordRequest : EXORpcRequest <NSCopying>
-@property(strong,nonatomic,readonly) NSArray *values;  // Array of EXORpcValues
-@property(copy,nonatomic,readonly) EXORpcRequestComplete complete;
+@property(strong,nonatomic,readonly) NSArray *values;  //// Array of EXORpcValues to record
+@property(copy,nonatomic,readonly) EXORpcRequestComplete complete; /// Callback when complete
 
+/**
+ Create a request to record a list of values
+ 
+ @param rid The resource to write the values to
+ @param value Array of EXORpcValues to be recorded
+ @param complete Callback when finished
+ @return The record request
+ */
 + (EXORpcRecordRequest*)recordWithRID:(EXORpcResourceID *)rid values:(NSArray*)values complete:(EXORpcRequestComplete)complete;
 
+/**
+ Initialize a request to record a list of values
+
+ @param rid The resource to write the values to
+ @param value Array of EXORpcValues to be recorded
+ @param complete Callback when finished
+ @return The record request
+ */
 - (instancetype)initWithRID:(EXORpcResourceID *)rid values:(NSArray*)values complete:(EXORpcRequestComplete)complete;
 
 @end
