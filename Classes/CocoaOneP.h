9ff604f5060b26c68a2988997bd4f79a1cd1c452
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2015-12-14 13:10:27 -0600

    Added Xcode project for library.

---
+++ Classes/CocoaOneP.h
@@ -0,0 +1,19 @@
+//
+//  CocoaOneP.h
+//  CocoaOneP
+//
+//  Created by Michael Conrad Tadpol Tilstra on 12/14/15.
+//  Copyright © 2015 Exosite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CocoaOneP.
+FOUNDATION_EXPORT double CocoaOnePVersionNumber;
+
+//! Project version string for CocoaOneP.
+FOUNDATION_EXPORT const unsigned char CocoaOnePVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaOneP/PublicHeader.h>
+
+
